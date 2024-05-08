4b56e7a1f8ef56826416e19d0d5280e66e27081a
dudurb17 <eduardo.robettibedin@gmail.com>
2024-05-08 11:05:24 -0300

    asserts

--- bin/models/account.dart
+++ bin/models/account.dart
@@ -2,11 +2,13 @@ class Account {
   String name;
   double balance;
   bool isAuthenticated;
-
+//asserts serve somente para produção, se der false ele vai parar e retornar a frase
   Account(
       {required this.name,
       required this.balance,
-      required this.isAuthenticated});
+      required this.isAuthenticated})
+      : assert(name.isEmpty, "O nome pode ser uma string vazia"),
+        assert(balance >= 0, "O valor nao pode ser menor q zero ou zero");
 
   editBalance({required value}) {
     balance = balance + value;
